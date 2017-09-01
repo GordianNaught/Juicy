c99feaa054892bd4e3c74b9a1f2125837fd8b8ec
Jonathan Avery <protist@localhost.localdomain>
2017-09-01 19:23:11 -0400

    forgot to add juicy_global.pl

---
+++ juicy_global.pl
@@ -0,0 +1,8 @@
+:- module(juicy_global, [assert_verbose/0, verbose/0, ifVerbose/1]).
+
+:- dynamic(verbose).
+:- meta_predicate(ifVerbose(?)).
+
+assert_verbose() :- assert(verbose).
+ifVerbose(Code) :- verbose, !, Code.
+ifVerbose(_Code).
