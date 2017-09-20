5e8c720a1101763e20cd2371810c803c2bce33f9
Jonathan Avery <protist@localhost.localdomain>
2017-09-19 22:48:52 -0400

    added more comments to juicy_swipl.pl

--- juicy_swipl.pl
+++ juicy_swipl.pl
@@ -1,5 +1,6 @@
-% this file is used to compile Juicy using SWI-Prolog
+%% This file is used to compile Juicy using SWI-Prolog.
 :- use_module(juicy,[start/1]).
 
+% This is the entry point for the compiled compiler.
 main(Arguments) :-
   start(Arguments).
